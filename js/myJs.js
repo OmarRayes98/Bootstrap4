72eab4f07bebb3e01ce6269b407e879cff7dbb1e
OmarRayes98 <omarrayes1998@gmail.com>
2023-03-30 02:19:00 +0200

    first comit

---
+++ js/myJs.js
@@ -0,0 +1,25 @@
+$(function(){
+    'use strict';
+    //Adjust slider height
+
+var winH = $(window).height(),
+upperH= $('.upper-bar').innerHeight(),
+navH= $('.navbar').innerHeight();
+
+$('.slider, .carousel-item').height(winH - (upperH +navH ));
+
+//Featured Work Shuffle
+
+$('.featured-work ul li').on('click',function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    console.log($(this).data('class'));
+    if($(this).data('class') === 'all'){
+      $('.shuffle-imgs .col-md').css('opacity','1'); 
+    }
+    else{
+        $('.shuffle-imgs .col-md').css('opacity','.08'); //or hidden
+        $($(this).data('class')).parent().css('opacity' ,1);
+    }
+});
+});
+
